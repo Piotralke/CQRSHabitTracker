3cb9c22dcf4cda6506231f97d38d4cbc37a8a3ee
piotr <piotr@DESKTOP-VHHME34>
2025-11-11 11:20:48 +0100

    Add project files.

---
+++ Mediator/Mediator.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Mediator
+{
+	public class Mediator : IMediator
+	{
+		private readonly IServiceProvider _serviceProvider;
+		public Mediator(IServiceProvider serviceProvider)
+		{
+			_serviceProvider = serviceProvider;
+		}
+		public async Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default) where TNotification : INotification
+		{
+			var handlerType = typeof(INotificationHandler<>).MakeGenericType(notification.GetType());
+			var handlers = _serviceProvider.GetServices(handlerType);
+
+			foreach (dynamic handler in handlers)
+			{
+				await handler.Handle((dynamic)notification, cancellationToken);
+			}
+		}
+
+		public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+		{
+			var handlerType = typeof(IRequestHandler<,>).MakeGenericType(request.GetType(), typeof(TResponse));
+			dynamic? handler = _serviceProvider.GetService(handlerType);
+
+			if (handler == null)
+				throw new InvalidOperationException($"Handler for {request.GetType().Name} not registered.");
+
+			return await handler.Handle((dynamic)request, cancellationToken);
+		}
+	}
+}
---
+++ Mediator/MediatorServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Mediator
+{
+	public static class MediatorServiceCollectionExtensions
+	{
+		public static  IServiceCollection AddMediator(this IServiceCollection services, params Assembly[] assemblies)
+		{
+			var types = assemblies.SelectMany(a => a.GetTypes());
+
+			//Pobieramy wszystkie klasy handlerów implementujące IRequestHandler<,> lub INotificationHandler<>
+			var handlers = types.Where(t =>
+				t.GetInterfaces().Any(i =>
+					i.IsGenericType &&
+					(i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>) ||
+					 i.GetGenericTypeDefinition() == typeof(INotificationHandler<>))));
+
+			//Pobieramy wszystkie interfejsy i rejestrujemy je w kontenerze DI
+			foreach (var handler in handlers)
+			{
+				var interfaces = handler.GetInterfaces()
+					.Where(i => i.IsGenericType &&
+						(i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>) ||
+						 i.GetGenericTypeDefinition() == typeof(INotificationHandler<>)));
+
+				foreach (var interf in interfaces)
+				{
+					services.AddTransient(interf, handler);
+				}
+			}
+			// Rejestrujemy Mediatora jako singleton
+			services.AddSingleton<IMediator, Mediator>();
+
+			return services;
+		}
+	}
+}
